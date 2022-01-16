5884f277c35bbbdbee2ec3c4d928a7872a078cd3
eqzhou81 <eqzhou81@gmail.com>
2022-01-15 17:25:22 -0800

    lots of new stuff

--- newTab.js
+++ newTab.js
@@ -19,3 +19,16 @@ function currentDate() {
 }
 
 currentDate();
+
+function changeBackground() {
+    var imgNumber = Math.floor((Math.random() * 6) + 1);
+    var images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg'];
+    $('html').css({'background-image': 'url(background-images/' + images[imgNumber]});
+}
+
+changeBackground();
+
+document.getElementById("enterName").addEventListener("click", function() {
+    var name = getElementById("name").value;
+    document.querySelector("h2").innerHTML = "Good " + timeGreeting + ", " + name + "!";
+})
