5f1d26890a41ee369cbb8e47f02ec9654312b72b
Darryl Tanzil <darryltanzil@gmail.com>
2022-01-15 16:26:43 -0800

    adding event handler

--- popup.js
+++ popup.js
@@ -3,15 +3,23 @@
 function postData() {
     
     // select input value 
-    var class = document.getElementById("class").value;
+    var _class = document.getElementById("class").value;
     var link = document.getElementById("link").value;
     var time = document.getElementById("time").value;
 
     // adds class, [link, time] to json
-    chrome.storage.sync.set({class: [link, time]}, function() {
-        console.log(`${class} is set to ${value}`);
+    chrome.storage.sync.set({_class: [link, time]}, function() {
+        console.log(`${_class} is set to ${value}`);
     });
 
+    alert("test");
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var submit = document.getElementById('submit');
+    // onClick's logic below:
+    submit.addEventListener('click', function() {
+        console.log("test");
+    });
+});
 
