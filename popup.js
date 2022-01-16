c556d501d9f7ce8f6b0d57b44d05b8b2c77918a7
Darryl Tanzil <darryltanzil@gmail.com>
2022-01-15 19:04:35 -0800

    scraper init

--- popup.js
+++ popup.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-=======
 // post the data to chrome.storage 
 function postData() {
     
